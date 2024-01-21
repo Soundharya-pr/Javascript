ccdb9927c9158ac3cea79038a2c5c39e4ed6052e
=Soundharya-pr <=soundharyapr14@gmail.com>
2024-01-21 21:36:32 +0530

    Javascript files

---
+++ 1.Variable.js
@@ -0,0 +1,9 @@
+console.log("We are starting JS now")
+const d = 100
+console.log("This is of const type const d=100 value is 100",d)
+y= 200 // If the same con
+console.log(y)
+var a =10
+console.log("This is of var declaration",a)
+let b ="Heloo folks"
+console.log("This is let declaration",b)
---
+++ 2.Operators.js
@@ -0,0 +1,19 @@
+//Arithmetic Operators
+let a = 0
+let b =100
+console.log(typeof a)
+console.log("Addition",a+b)
+console.log("Subtraction",a-b)
+console.log("Multiplication",a*b)
+console.log("Division",a/b)
+console.log("Modulo",a%b)
+//Logical
+
+console.log("And",a&&b)
+console.log("OR",a||b)
+console.log("NOT",!a)
+console.log(10||20)
+console.log(0||20)
+console.log(100||20 && 0)
+console.log((100||20)&&0)
+console.log(!a&&b)
