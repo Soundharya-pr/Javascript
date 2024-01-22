46b9fa0c8506b4cf94dec7baeca5eb80161856af
=Soundharya-pr <=soundharyapr14@gmail.com>
2024-01-22 11:25:07 +0530

    JS file Day3

---
+++ 5.Arrays.js
@@ -0,0 +1,29 @@
+/*arr =[10,20,30,40]
+console.log(arr.length)
+for(var i =0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
+
+arr =[10,20,30,40.444]
+console.log(arr.length)
+for(var i =0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
+
+arr =[10,20,30,"true"]
+console.log(arr.length)
+for(var i =0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
+
+arr =[10,"Twenty",30,40,"Fifty"]
+console.log(arr.length)
+for(var i =0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
+*/
+
