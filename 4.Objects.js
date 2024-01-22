c32e6c1ce1468e1b3649cc4b0fd76584698185ac
=Soundharya-pr <=soundharyapr14@gmail.com>
2024-01-22 11:35:57 +0530

    JS Day 3

---
+++ 4.Objects.js
@@ -0,0 +1,24 @@
+var Details = {
+    "Name" : "Soundharya",
+    "Age" : 20,
+    "Phone" : "9047440900"
+
+}
+Details["ID"] = "21ECR199"
+console.log(Details) 
+
+var FoodBill = {}
+FoodBill["Item"] = "M - Biriyani"
+FoodBill["Price"] = 200
+FoodBill["Count"] = 2
+FoodBill["Hotel"] = "Venu Biriyani"
+console.log(FoodBill)     
+ 
+var kongu = new Object()
+kongu["Fees"] = "1 L"
+kongu["Accomodation"] = "Average"
+kongu["Hostelcount"] = 10
+kongu["Environment"] = "Greenish"
+console.log(kongu)
+console.log(kongu["Fees"])
+console.log(kongu.Environment)
